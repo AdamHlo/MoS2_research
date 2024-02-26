b9c445fb65a598904e2e8459a7484a7672965250
Adam Hlozny <adam@pop-os.localdomain>
2024-02-26 14:17:50 +0100

    Improve organize

--- pw/organize.sh
+++ pw/organize.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
 DT=`date +"%d-%b-%Y_%H-%M-%S"`
-OUTDIR="./results/$DT/out"
-DIR="./results/$DT/"
+OUTDIR="./results/${DT}_$1/out"
+DIR="./results/${DT}_$1/"
+
+echo "created: $DIR"
 
 mkdir -p $OUTDIR
 
 if [ -d ./out ]; then
   cp -r ./out "$DIR/out"
   rm -r ./out/*
+fi
+
+if [ -f CRASH ]; then
+  mv CRASH "$DIR/out/CRASH"
 fi
